135c431c37d241a893edeeadb32c944b98fc2d5c
George Redinger <george@georgeredinger.com>
2014-02-05 12:34:10 -0800

    start conversion into a data logger

--- build/source/r/debug/com/redbear/chat/R.java
+++ build/source/r/debug/com/redbear/chat/R.java
@@ -19,11 +19,9 @@ public final class R {
         public static final int redbear=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
+        public static final int action_settings=0x7f080004;
         public static final int btn=0x7f080002;
-        public static final int editText=0x7f080004;
         public static final int pageloading=0x7f080000;
-        public static final int send=0x7f080005;
         public static final int textView=0x7f080003;
         public static final int tv=0x7f080001;
     }
--- src/com/redbear/chat/Chat.java
+++ src/com/redbear/chat/Chat.java
@@ -26,172 +26,155 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class Chat extends Activity {
-	private final static String TAG = Chat.class.getSimpleName();
+    private final static String TAG = Chat.class.getSimpleName();
 
-	public static final String EXTRAS_DEVICE = "EXTRAS_DEVICE";
-	private BluetoothDevice device;
-	private TextView tv = null;
-	private EditText et = null;
-	private Button btn = null;
-	private String mDeviceName;
-	private String mDeviceAddress;
-	private RBLService mBluetoothLeService;
-	private Map<UUID, BluetoothGattCharacteristic> map = new HashMap<UUID, BluetoothGattCharacteristic>();
+    public static final String EXTRAS_DEVICE = "EXTRAS_DEVICE";
+    private BluetoothDevice device;
+    private TextView tv = null;
+    private EditText et = null;
+    private Button btn = null;
+    private String mDeviceName;
+    private String mDeviceAddress;
+    private RBLService mBluetoothLeService;
+    private Map<UUID, BluetoothGattCharacteristic> map = new HashMap<UUID, BluetoothGattCharacteristic>();
 
-	private final ServiceConnection mServiceConnection = new ServiceConnection() {
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
 
-		@Override
-		public void onServiceConnected(ComponentName componentName,
-				IBinder service) {
-			mBluetoothLeService = ((RBLService.LocalBinder) service)
-					.getService();
-			if (!mBluetoothLeService.initialize()) {
-				Log.e(TAG, "Unable to initialize Bluetooth");
-				finish();
-			}
-			// Automatically connects to the device upon successful start-up
-			// initialization.
-			mBluetoothLeService.connect(mDeviceAddress);
-		}
+        @Override
+        public void onServiceConnected(ComponentName componentName,
+                IBinder service) {
+            mBluetoothLeService = ((RBLService.LocalBinder) service)
+                    .getService();
+            if (!mBluetoothLeService.initialize()) {
+                Log.e(TAG, "Unable to initialize Bluetooth");
+                finish();
+            }
+            // Automatically connects to the device upon successful start-up
+            // initialization.
+            mBluetoothLeService.connect(mDeviceAddress);
+        }
 
-		@Override
-		public void onServiceDisconnected(ComponentName componentName) {
-			mBluetoothLeService = null;
-		}
-	};
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mBluetoothLeService = null;
+        }
+    };
 
-	private final BroadcastReceiver mGattUpdateReceiver = new BroadcastReceiver() {
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			final String action = intent.getAction();
+    private final BroadcastReceiver mGattUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
 
-			if (RBLService.ACTION_GATT_DISCONNECTED.equals(action)) {
-			} else if (RBLService.ACTION_GATT_SERVICES_DISCOVERED
-					.equals(action)) {
-				getGattService(mBluetoothLeService.getSupportedGattService());
-			} else if (RBLService.ACTION_DATA_AVAILABLE.equals(action)) {
-				displayData(intent.getByteArrayExtra(RBLService.EXTRA_DATA));
-			}
-		}
-	};
+            if (RBLService.ACTION_GATT_DISCONNECTED.equals(action)) {
+            } else if (RBLService.ACTION_GATT_SERVICES_DISCOVERED
+                    .equals(action)) {
+                getGattService(mBluetoothLeService.getSupportedGattService());
+            } else if (RBLService.ACTION_DATA_AVAILABLE.equals(action)) {
+                displayData(intent.getByteArrayExtra(RBLService.EXTRA_DATA));
+            }
+        }
+    };
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.second);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
 
-		tv = (TextView) findViewById(R.id.textView);
-		tv.setMovementMethod(ScrollingMovementMethod.getInstance());
-		et = (EditText) findViewById(R.id.editText);
-		btn = (Button) findViewById(R.id.send);
-		btn.setOnClickListener(new OnClickListener() {
+        tv = (TextView) findViewById(R.id.textView);
+        tv.setMovementMethod(ScrollingMovementMethod.getInstance());
 
-			@Override
-			public void onClick(View v) {
-				BluetoothGattCharacteristic characteristic = map
-						.get(RBLService.UUID_BLE_SHIELD_TX);
 
-				String str = et.getText().toString() + "\r\n";
-				final byte[] tx = str.getBytes();
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        device = bundle.getParcelable(EXTRAS_DEVICE);
 
-				characteristic.setValue(tx);
-				mBluetoothLeService.writeCharacteristic(characteristic);
+        mDeviceAddress = device.getAddress();
+        mDeviceName = device.getName();
 
-				et.setText("");
-			}
-		});
+        getActionBar().setTitle(mDeviceName);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
 
-		Intent intent = getIntent();
-		Bundle bundle = intent.getExtras();
-		device = bundle.getParcelable(EXTRAS_DEVICE);
+        Intent gattServiceIntent = new Intent(this, RBLService.class);
+        bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
+    }
 
-		mDeviceAddress = device.getAddress();
-		mDeviceName = device.getName();
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-		getActionBar().setTitle(mDeviceName);
-		getActionBar().setDisplayHomeAsUpEnabled(true);
+        registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
+    }
 
-		Intent gattServiceIntent = new Intent(this, RBLService.class);
-		bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            mBluetoothLeService.disconnect();
+            mBluetoothLeService.close();
 
-	@Override
-	protected void onResume() {
-		super.onResume();
+            System.exit(0);
+        }
 
-		registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
-	}
+        return super.onOptionsItemSelected(item);
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		if (item.getItemId() == android.R.id.home) {
-			mBluetoothLeService.disconnect();
-			mBluetoothLeService.close();
+    @Override
+    protected void onStop() {
+        super.onStop();
 
-			System.exit(0);
-		}
+        unregisterReceiver(mGattUpdateReceiver);
+    }
 
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
 
-	@Override
-	protected void onStop() {
-		super.onStop();
+        mBluetoothLeService.disconnect();
+        mBluetoothLeService.close();
 
-		unregisterReceiver(mGattUpdateReceiver);
-	}
+        System.exit(0);
+    }
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
+    private void displayData(byte[] byteArray) {
+        if (byteArray != null) {
+            String data = new String(byteArray);
+            tv.append(data);
+            // find the amount we need to scroll. This works by
+            // asking the TextView's internal layout for the position
+            // of the final line and then subtracting the TextView's height
+            final int scrollAmount = tv.getLayout().getLineTop(
+                    tv.getLineCount())
+                    - tv.getHeight();
+            // if there is no need to scroll, scrollAmount will be <=0
+            if (scrollAmount > 0)
+                tv.scrollTo(0, scrollAmount);
+            else
+                tv.scrollTo(0, 0);
+        }
+    }
 
-		mBluetoothLeService.disconnect();
-		mBluetoothLeService.close();
+    private void getGattService(BluetoothGattService gattService) {
+        if (gattService == null)
+            return;
 
-		System.exit(0);
-	}
+        BluetoothGattCharacteristic characteristic = gattService
+                .getCharacteristic(RBLService.UUID_BLE_SHIELD_TX);
+        map.put(characteristic.getUuid(), characteristic);
 
-	private void displayData(byte[] byteArray) {
-		if (byteArray != null) {
-			String data = new String(byteArray);
-			tv.append(data);
-			// find the amount we need to scroll. This works by
-			// asking the TextView's internal layout for the position
-			// of the final line and then subtracting the TextView's height
-			final int scrollAmount = tv.getLayout().getLineTop(
-					tv.getLineCount())
-					- tv.getHeight();
-			// if there is no need to scroll, scrollAmount will be <=0
-			if (scrollAmount > 0)
-				tv.scrollTo(0, scrollAmount);
-			else
-				tv.scrollTo(0, 0);
-		}
-	}
+        BluetoothGattCharacteristic characteristicRx = gattService
+                .getCharacteristic(RBLService.UUID_BLE_SHIELD_RX);
+        mBluetoothLeService.setCharacteristicNotification(characteristicRx,
+                true);
+        mBluetoothLeService.readCharacteristic(characteristicRx);
+    }
 
-	private void getGattService(BluetoothGattService gattService) {
-		if (gattService == null)
-			return;
+    private static IntentFilter makeGattUpdateIntentFilter() {
+        final IntentFilter intentFilter = new IntentFilter();
 
-		BluetoothGattCharacteristic characteristic = gattService
-				.getCharacteristic(RBLService.UUID_BLE_SHIELD_TX);
-		map.put(characteristic.getUuid(), characteristic);
+        intentFilter.addAction(RBLService.ACTION_GATT_CONNECTED);
+        intentFilter.addAction(RBLService.ACTION_GATT_DISCONNECTED);
+        intentFilter.addAction(RBLService.ACTION_GATT_SERVICES_DISCOVERED);
+        intentFilter.addAction(RBLService.ACTION_DATA_AVAILABLE);
 
-		BluetoothGattCharacteristic characteristicRx = gattService
-				.getCharacteristic(RBLService.UUID_BLE_SHIELD_RX);
-		mBluetoothLeService.setCharacteristicNotification(characteristicRx,
-				true);
-		mBluetoothLeService.readCharacteristic(characteristicRx);
-	}
-
-	private static IntentFilter makeGattUpdateIntentFilter() {
-		final IntentFilter intentFilter = new IntentFilter();
-
-		intentFilter.addAction(RBLService.ACTION_GATT_CONNECTED);
-		intentFilter.addAction(RBLService.ACTION_GATT_DISCONNECTED);
-		intentFilter.addAction(RBLService.ACTION_GATT_SERVICES_DISCOVERED);
-		intentFilter.addAction(RBLService.ACTION_DATA_AVAILABLE);
-
-		return intentFilter;
-	}
+        return intentFilter;
+    }
 }
